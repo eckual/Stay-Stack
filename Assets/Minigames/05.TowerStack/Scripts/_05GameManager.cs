2ce9bcdd276ed67d51166c968b25f3bd9cb14c43
BahaHadyaoui <67714398+eckual@users.noreply.github.com>
2024-11-11 11:34:48 +0100

    Update _05GameManager.cs

--- Assets/Minigames/05.TowerStack/Scripts/_05GameManager.cs
+++ Assets/Minigames/05.TowerStack/Scripts/_05GameManager.cs
@@ -5,6 +5,7 @@ using Essentials;
 public class _05GameManager : MonoBehaviour
 {
     public bool spawnOnZ = true;
+    
     private void OnEnable()
     {
         // InputManager._SprintEvent += OnSprint;
@@ -18,20 +19,15 @@ public class _05GameManager : MonoBehaviour
         InputManager.Instance._Fire1Event -= OnFire;
     }
 
-
     private void OnFire()
     {
-        Debug.Log("Fire 1 Pressed");
         if (_05MovingPlatform.CurrentPlatform)
         {
             _05MovingPlatform.CurrentPlatform.Stop();
 
             Camera.main.transform.position += new Vector3(0, 0.1f, 0);
             spawnOnZ = !spawnOnZ;
-            // _05MovingPlatform.LastPlatform = _05MovingPlatform.CurrentPlatform;
-            // _05MovingPlatform.CurrentPlatform = platform.GetComponent<_05MovingPlatform>();
         }
-
     }
 
 }
